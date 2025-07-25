f52ac3df126a92b5d0f54f5dc201ffe743dac59e
marce <marcelo.macedo01@hotmail.com>
2025-07-25 19:57:30 -0300

    3

--- microservices/legal-usuario/src/main/java/br/com/legalconnect/advogado/application/dto/enums/DocumentoTipo.java
+++
@@ -1,9 +0,0 @@
-package br.com.legalconnect.advogado.application.dto.enums;
-
-public enum DocumentoTipo {
-    OAB, // Ordem dos Advogados do Brasil
-    RG, // Registro Geral (identidade)
-    CPF, // Cadastro de Pessoas Físicas
-    COMPROVANTE_ENDERECO, // Comprovante de residência
-    OUTRO // Outros tipos de documentos não listados explicitamente
-}
