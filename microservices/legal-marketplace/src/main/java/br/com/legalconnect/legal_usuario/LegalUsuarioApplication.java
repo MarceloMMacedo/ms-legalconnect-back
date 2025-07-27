a4df1cca0a28e11930bc040b5fc5855530167fb6
marce <marcelo.macedo01@hotmail.com>
2025-07-27 11:20:41 -0300

    43

---
+++ microservices/legal-marketplace/src/main/java/br/com/legalconnect/legal_usuario/LegalUsuarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.legalconnect.legal_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalUsuarioApplication.class, args);
+	}
+
+}
